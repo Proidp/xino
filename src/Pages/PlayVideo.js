8f418f197777cd1c267533dd020be941ed84a105
Prodip Chandra Barman <78926310+Proidp@users.noreply.github.com>
2022-02-06 18:29:17 +0600

    Add files via upload

---
+++ src/Pages/PlayVideo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import Navbar from './Navbar';
+// import SingleNavbar from './SingleNavbar';
+
+
+const PlayVideo = () => {
+  return (
+      <>
+      <Navbar/>
+      
+      </>
+  )
+};
+
+export default PlayVideo;
